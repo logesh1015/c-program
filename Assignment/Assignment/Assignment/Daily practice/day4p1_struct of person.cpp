546c44acb42d35ed3030aa590dfb4473d35f4c5b
logesh1015 <129208889+logesh1015@users.noreply.github.com>
2023-04-01 09:33:23 +0530

    Create day4p1_struct of person.cpp

---
+++ Assignment/Assignment/Assignment/Daily practice/day4p1_struct of person.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+struct person
+{#include<stdio.h>
+	char name[20];
+	int age;
+	float height;
+} p1;
+main()
+{
+	scanf("%s %d %f",p1.name,&p1.age,&p1.height);
+    printf("%s %d %.2f",p1.name,p1.age,p1.height);
+
+}
